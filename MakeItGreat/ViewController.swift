0a5f6dc9cb8e15a5e6e06e441b537bc9fad524ca
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2022-11-21 09:03:37 -0300

    Initial Commit

---
+++ MakeItGreat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeItGreat
+//
+//  Created by Lucas Barbosa de Oliveira on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
