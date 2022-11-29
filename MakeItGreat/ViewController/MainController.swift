a332ee0219e3cefd95aa310c1b836d54d7619de1
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2022-11-29 10:53:04 -0300

    Criacao da tela principal em padrao de projeto MVC

--- MakeItGreat/ViewController/MainController.swift
+++ MakeItGreat/ViewController/MainController.swift
@@ -1,20 +1,11 @@
-//
-//  MainController.swift
-//  MakeItGreat
-//
-//  Created by Lucas Barbosa de Oliveira on 29/11/22.
-//
-
 import UIKit
 
 class MainController: UIViewController {
 
     let mainView = MainView()
-    
     override func loadView() {
         view = mainView
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
